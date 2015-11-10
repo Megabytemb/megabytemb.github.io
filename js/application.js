97179b3278c7b752d1c0d385b7c8495e5eb8e3ac
Michael Benz <michael@benz.io>
2015-11-10 15:23:17 +1100

    Initial Material Design with Angular Material

--- js/application.js
+++ js/application.js
@@ -1,1 +1,12 @@
-// Your JavaScript
+var app = angular.module('BlargApp', ['ngMaterial']);
+
+app.run(function() {
+    $("h1").addClass('md-title');
+});
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
+    $scope.toggleSidenav = function (menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+
+}]);
